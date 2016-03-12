7175e4b6f648ec49c94d153f60805b271556877d
Shehryar <shehryar2212@yahoo.com>
2016-03-12 13:55:20 -0500

    navbar styling, foundation

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'zyka',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -119,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT= 'staticfiles'
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR, "static")
+]
